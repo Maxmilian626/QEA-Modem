90b77274bd41e780ed159d306cf8c202cfce270e
Maxmilian626 <maxmilian.wei@students.olin.edu>
2016-09-19 11:16:18 -0400

    started the setup

--- BinaryToAudio.py
+++ BinaryToAudio.py
@@ -1,1 +1,17 @@
+import numpy as np
 import scipy.io.wavfile
+import matplotlib
+import math
+
+
+#Sound generation parameters
+Fs = 10000 # Sampling Rate, in hertz -> samples/second, 44100 is standard
+ref_note = 440 #This is hertz, A note.
+rps = (2*math.pi)/Fs #rads/sample
+
+
+#Square Wave Generation
+ones = np.ones(300)
+neg_ones = -1*np.ones(300)
+
+test_signal = np.append(ones, neg_ones, ones)
