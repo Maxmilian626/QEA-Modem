5acc38a91ca6ada40fe1f35e09f1c254a90a68e3
margaretmcrawf <margaretmcrawf@gmail.com>
2016-09-21 20:46:36 -0400

    working on reversing BPSK

--- receiver.py
+++ receiver.py
@@ -4,13 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pyaudio
 import wave
-import sys
-from multiprocessing import Process, Queue
-import time
-import json
-import requests
-import wave
-import evdev
 import struct
 import itertools
 import scipy.fftpack
@@ -19,7 +12,6 @@ mrs2str = {'113': 'u', '111': 's', '13': 'a', '131': 'r', '11': 'i', '133': 'w',
 '33': 'm', '1111': 'h', '1113': 'v', '1': 'e', '1311': 'l', '3': 't', '1131': 'f',
  '1331': 'p', '1333': 'j', '3131': 'c', '3133': 'y', '3311': 'z', '3313': 'q', '3113': 'x', '3111': 'b', 
  '333': 'o', '331': 'g', '311': 'd', '313': 'k'}
-
 
 def lowPass(filterorder, sig):
 
@@ -34,30 +26,38 @@ def movingAverage (values, window):
 	sma = np.convolve(values, weights, 'valid')
 	return sma
 
-def bits(wave):
-	amp_threshold = 7 #this is a placeholder
-	dash_threshold = 1000 #distinction between dot and dash
-	new_threshold = 3000 #threshold to be a new letter
-	chunks = isplit(wave, (amp_threshold,))
-	letterlist = []
-	for chunk in chunks:
-		if chunk[0] > amp_threshold: #dot or dash
-			if len(chunk) > dash_threshold:
-				letterlist.append("3")
-			else:
-				letterlist.append("1")
-		else: #long pause or short pause
-			if len(chunk) > new_threshold:
-				letterlist.append("0")
-			#else do nothing
-	return ("".join(letterlist).split("0"))
+# def bits(wave):
+# 	amp_threshold = 7 #this is a placeholder. it's totally too high.
+# 	dash_threshold = 1000 #distinction between dot and dash
+# 	new_threshold = 3000 #threshold to be a new letter
+# 	chunks = isplit(wave, (amp_threshold,))
+# 	letterlist = []
+# 	for chunk in chunks:
+# 		if chunk[0] > amp_threshold: #dot or dash
+# 			if len(chunk) > dash_threshold:
+# 				letterlist.append("3")
+# 			else:
+# 				letterlist.append("1")
+# 		else: #long pause or short pause
+# 			if len(chunk) > new_threshold:
+# 				letterlist.append("0")
+# 			#else do nothing
+# 	return ("".join(letterlist).split("0"))
 
 def isplit(iterable, splitters):
 	#a very nice split function, splits the iterable(list) on the splitter value
 	return [list(g) for k,g in itertools.groupby(iterable,lambda x:x in splitters) if not k]
 
-def letters(bits):
-	pass
+def downconversion(signal):
+	Fs = 44100 # Sampling Rate, in hertz -> samples/second, 44100 is standard
+	Ts = 1.0/Fs # sampling interval
+	Carrier_Frequency = 16. #This is hertz, A note.  The carrier frequency, I guess
+	Fc = 1.0/Carrier_Frequency
+
+	domain = np.arange(len(signal))
+	time = Ts*np.array(domain) #time array
+
+	return (5.0)*np.cos(2.0*math.pi*Fc*time) # 5 is placeholder for amplitude...
 
 
 def record():
@@ -108,15 +108,17 @@ if __name__ == '__main__':
 
 	lowpassed = lowPass(10, audio)
 
-	smooth_magnitude = movingAverage(abs(lowpassed), 5) #takes the absolute value, then a moving average of that
+	back = downconversion(lowpassed)
 
-	bits = bits(smooth_magnitude)
+	# smooth_magnitude = movingAverage(abs(lowpassed), 5) #takes the absolute value, then a moving average of that
 
-	word = ""
-	for bit in bits:
-		word += mrs2str[bit]
+	# bits = bits(smooth_magnitude)
 
-	print word
+	# word = ""
+	# for bit in bits:
+	# 	word += mrs2str[bit]
+
+	# print word
 
 	Aud = np.array(scipy.fftpack.fft(audio))
 	Low = np.array(scipy.fftpack.fft(lowpassed))
