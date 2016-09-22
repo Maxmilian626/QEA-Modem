3e83b9804d88860fbd76c8e1bf70d93a9ecd4f06
Maxmilian626 <maxmilian.wei@students.olin.edu>
2016-09-21 20:15:36 -0400

    Got the wave modulation working

--- BinaryToAudio.py
+++ BinaryToAudio.py
@@ -3,18 +3,22 @@ import scipy.io.wavfile
 import scipy.fftpack
 import matplotlib.pyplot
 import math
+import binascii
 
 
 #Sound generation parameters
-Fs = 300 # Sampling Rate, in hertz -> samples/second, 44100 is standard
+signal_length = 30. #ms
+Fs = 1/signal_length # Sampling Rate, in hertz -> samples/second, 44100 is standard
 Ts = 1.0/Fs # sampling interval
-ref_note = 440 #This is hertz, A note.  The carrier wave, I guess
-rps = (2*math.pi)/Fs #rads/sample
+Carrier_Frequency = 16. #This is hertz, A note.  The carrier frequency, I guess
+Fc = 1.0/Carrier_Frequency
+#ps = (2*math.pi)/Fs #rads/sample
+
 
 
 #Square Wave Generation
-ones = np.ones(300)
-neg_ones = -1*np.ones(300)
+ones = np.ones(signal_length)
+neg_ones = -1.0*np.ones(signal_length)
 
 test_data = np.append(ones, np.append(neg_ones, ones))
 
@@ -28,13 +32,15 @@ test_data = np.append(ones, np.append(neg_ones, ones))
 
 #Convert to Sound signal
 domain = np.arange(len(test_data))
-time = Ts*np.array(domain)
-omegaX = rps * np.array(domain)
+time = Ts*np.array(domain) #time array
+
+amplitude = math.sqrt(2/Fs)
+omegaX = (5.0)*np.cos(2.0*math.pi*Fc*time) #cos(2pi*Fs*n)
 print omegaX
-test_signal = math.cos(omegaX)*test_data
+test_signal = np.multiply(omegaX, test_data) #multiplies by 1 or -1
 
 test_signal_fft = scipy.fftpack.fft(test_signal)
-matplotlib.pyplot.plot(time, np.absolute(test_signal_fft))
+matplotlib.pyplot.plot(time, test_signal)
 matplotlib.pyplot.show()
 
 #scipy.io.wavfile.write('test.wav', Fs, test_signal)
